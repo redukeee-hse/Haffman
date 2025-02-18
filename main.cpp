6e4c067de95c45ea174fd6dc2525dac57caeaacf
anyabanyanya <anabugakova0760@gmail.com>
2025-02-18 15:05:47 +0300

    start v1.1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <map>
 #include <vector>
+#include <string>
 using namespace std;
 int main() {
     FILE* file = fopen("saratov.txt", "rb");
@@ -10,14 +11,18 @@ int main() {
         exit(1);
     }
     fseek(file, 0, SEEK_END);
-    long dlina = ftell(file);
+    long leng = ftell(file);
     fseek(file, 0, SEEK_SET);
     map<char,int> periodich;
-    for (int i = 0; i < dlina; ++i)
+    vector<pair<char, int>> sort_freq;
+    for (int i = 0; i < leng; ++i)
     {
         periodich[fgetc(file)]++;
     }
     for (auto r = periodich.begin(); r!=periodich.end(); r++){
+        sort_freq.push_back({r->first, r->second});
+    }
+    for (auto r = sort_freq.begin(); r!=sort_freq.end(); r++){
         cout << r->first << " "<< r->second << endl;
     }
     fclose(file);
